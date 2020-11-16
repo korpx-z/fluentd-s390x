8819e90f1c20f447064f36fb5f00a9e70c164980
Charles Korpics <Charles.Korpics@ibm.com>
2020-11-16 11:40:36 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ echo -e "\n $ANSI_YELLOW *** testing docker run - fluentd  *** $ANSI_RESET \n"
 echo -e "$ANSI_YELLOW is fluentd listening/running?: $ANSI_RESET"
 
 docker volume create fluentvol
-docker run -d -p 24224:24224 -p 24224:24224/udp -v fluentvol:/fluentd/log --name some-flu fluentd
+docker run -d -p 24224:24224 -p 24224:24224/udp -v fluentvol:/fluentd/log --name some-flu quay.io/ibmz/fluentd:1.10
 docker logs some-flu | grep 'fluentd worker is now running worker=0'
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
