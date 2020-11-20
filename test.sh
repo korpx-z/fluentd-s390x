3d2612795443eeeb1a9a00530a75bc955f9918c9
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-11-20 14:42:05 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,8 +8,7 @@ echo -e "\n $ANSI_YELLOW *** testing docker run - fluentd  *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW is fluentd listening/running?: $ANSI_RESET"
 
-docker volume create fluentvol
-docker run -d -p 24224:24224 -p 24224:24224/udp -v fluentvol:/fluentd/log --name some-flu quay.io/ibmz/fluentd:1.10
-docker logs some-flu 
+docker run --name Mule -d -p 8081:7777  quay.io/ibmz/mule:3.9.1 mule start
+docker logs Mule 
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
